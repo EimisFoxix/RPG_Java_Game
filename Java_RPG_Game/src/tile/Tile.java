5330df05460440d758acfbae0587e56ba6d77c68
Foxix <Foxix@Foxix>
2023-01-16 00:36:16 +0000

    Revert "Third commit, more sprites, bug fixes, refactoring the code towards more object-oriented framework."
    This reverts commit 3038189eb016030c175e0d15df7bf9a3b362396c.

--- Java_RPG_Game/src/tile/Tile.java
+++ Java_RPG_Game/src/tile/Tile.java
@@ -4,34 +4,7 @@ import java.awt.image.BufferedImage;
 
 public class Tile {
 
-	BufferedImage image;
-	boolean collision = false;
-	boolean pushBack = false;
-	boolean damage = false;
-	boolean grippy = false;
-	
-	public Boolean getCollision() {
-		return collision;
-	}
-	public Boolean getPushBack() {
-		return pushBack;
-	}
-	public Boolean getDamage() {
-		return damage;
-	}
-	public Boolean getGrippy() {
-		return grippy;
-	}
-	public void setCollision(boolean value) {
-		collision = value;
-	}
-	public void setPushBack(boolean value) {
-		pushBack = value;
-	}
-	public void setDamage(boolean value) {
-		damage = value;
-	}
-	public void setGrippy(boolean value) {
-		grippy = value;
-	}
+	public BufferedImage image;
+	public boolean collision = false;
+	public boolean pushBack = false;
 }
